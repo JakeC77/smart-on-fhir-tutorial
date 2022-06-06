648980066da6bd16841ebb4b0be14402b2a72055
JakeC77 <jake0christensen@gmail.com>
2022-06-06 14:14:08 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -16,7 +16,7 @@
                    
                   });
         var enc = smart.patient.api.fetchAll({
-                    type: 'Immunization',
+                    type: 'Condition',
                   });
 
         $.when(pt, obv).fail(onError);
